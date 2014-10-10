3d3d923082a5519d7be0dec14f61ca3b07dd9963
unknown <croftnotworking@gmail.com>
2014-10-10 10:34:49 +0100

    test suite bootstrapped

---
+++ client/tests/app.test.js
@@ -0,0 +1,10 @@
+describe("Unit: eshoprShop", function() {
+  beforeEach(module('eshoprShop'));
+  // var a;
+
+  // it("and so is a spec", function() {
+  //   a = true;
+
+  //   expect(a).toBe(true);
+  // });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,70 @@
-module.exports = function(config){
-    config.set({
-        basePath : 'client',
+// Karma configuration
+// Generated on Fri Oct 10 2014 09:13:59 GMT+0100 (GMT Daylight Time)
 
-        files : [
-            'components/angular/angular.js',
-            'components/angular-cookies/angular-cookies.js',
-            'components/angular-mocks/angular-mocks.js',
-            'components/angular-ui-router/release/angular-ui-router.js',
-            'js/**/*.js',
-            'tests/unit/**/*.js'
-        ],
+module.exports = function(config) {
+  config.set({
 
-        /*exclude : [
-            'app/lib/angular/angular-loader.js',
-            'app/lib/angular/*.min.js',
-            'app/lib/angular/angular-scenario.js'
-        ],*/
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-        autoWatch : true,
 
-        frameworks: ['mocha', 'chai'],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-        browsers : ['Chrome'],
 
-        plugins : [
-            'karma-junit-reporter',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-mocha',
-            'karma-chai'
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      'client/components/angular/angular.js',
+      'client/components/angular-mocks/angular-mocks.js',
+      // 'client/components/**/*.js',
+      'client/js/app.js',
+      // 'client/js/**/*.js',
+      'client/tests/**/*.js'
+    ],
 
-        junitReporter : {
-            outputFile: 'test_out/unit.xml',
-            suite: 'unit'
-        }
 
-    })
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
 };
